d2ba91cb85683a9cd5f3f8bd7588ad3f115973eb
Shivaditya Gohil <sgohil3@illinois.edu>
2022-11-15 09:13:35 +0000

    finished writing the constructor, and all of its helper fn except for connectGraphs(). Should be completed after that

--- src/fileParser.cpp
+++ src/fileParser.cpp
@@ -5,37 +5,110 @@
 #include <fstream>
 #include <string>
 
-fullGraph::fullGraph(const std::string& transport1, const std::string& transport2, const std::string& travelEdge ){
+fullGraph::fullGraph(const std::string& transport1, const std::string& transport2, const std::string& travelEdge1, const std::string& travelEdge2 ){
     
-    std::vector<std::pair<double,double>> allPoints; // A vector to hold our vertices of the 2nd transport, this will be used to make our KDtree
+    transport2_end = insertAllNodes(transport2, true); // inserts the two types of transport nodes and get what indexes they will end/start at
+    transport1_start = transport2_end +1;
+    int dummy = insertAllNodes(transport1, false);
+
+    dummy = insertAllEdges(travelEdge1); // we know insert our two data sets of edges
+    dummy = insertAllEdges(travelEdge2);
+
+    connectGraphs(); // we will know take our two connected graphs and make one connected graph out of it
+
+}
+
+int fullGraph::insertAllNodes(const std::string& nodeFile, bool needMap) {
+
     string tempWord; // a temp string that holds the data from our files
-
-   
-   
-    std::fstream Nodes_2 (transport2, std::fstream::out);// opens our 2nd file for reading
-
-    if(!Nodes_2.is_open()) { // if the file was unsuccesfully read then cancel the fn
-        return;
+    int i = 0; // a counter variable that will keep track of how many nodes have been added
+    std::fstream Nodes_ (nodeFile, std::fstream::out);// opens our 2nd file for reading
+    if(!Nodes_.is_open()) { // if the file was unsuccesfully read then cancel the fn
+        return -1;
     } else {
-        while(Nodes_2 >> tempWord){ // we will loop through the entire file
+        while(Nodes_ >> tempWord){ // we will loop through the entire file
         //however we will pull the word 3 more times within the loop in order to create our Node as the data has already been placed in the necesarry format
         
-            int ID = std::stoi(tempWord); // using stoi to convert string to int and stod to convert string to double, we can get the data from the file and place it as a node 
-            Nodes_2>>tempWord;
-            double longitude,latitude;
-            longitude = std::stod(tempWord);
-            Nodes_2>>tempWord;
-            latitude = std::stod(tempWord);
-            std::pair <double,double> coords (longitude, latitude); 
-            
-            bool result = fullGraph_.insertNode(ID, coords); // insert the node into the graph and 
-            allPoints.push_back(coords);
+            int ID = std::stoi(tempWord); // using stoi to convert string to int 
+            Nodes_>>tempWord;
 
+            if(tempWord.compare("0")!=0) { // if the data pulled is of the 1st travel type ie: airport, this will check if we have a name or not
+            //talk to bm of how to implement this since Node only has ID/coords/adjList
+            }
+
+            Nodes_>>tempWord;
+            double longitude,latitude;
+            longitude = std::stod(tempWord); // use stod to convert string to double, we can get the data from the file and place it as a node 
+            Nodes_>>tempWord;
+            latitude = std::stod(tempWord);
+            std::pair <double,double> coords (longitude, latitude); // then place that node int our coordinate pair
+            
+            if(needMap) { // if we denoted a need for a map, then add the coordiante/ID to the map
+                coorToID[coords] = ID;
+            }
+            
+            bool result = fullG_.insertNode(ID, coords); // insert the node into the graph and add it to our vector
+            allPoints.push_back(coords);
+            i++; 
         }
     }
+    Nodes_.close(); // close the current file at reading
+    return i-1; // this returns the last index of our Nodes, if it is negative one then no nodes were added
+}
 
-    
 
-    
+int fullGraph::insertAllEdges(const std::string& edgefile) {
 
+    string tempWord; // a temp string that holds the data from our files
+    int sourceNode; // these two string are temp vars that will hold the source and destination node for our edges
+    int destinationNode;
+    int i = 0; // a counter variable that will keep track of how many edges have been added
+    std::fstream Edges_ (edgefile, std::fstream::out);// opens our 2nd file for reading
+
+    if(!Edges_.is_open()) { // if the file was unsuccesfully read then cancel the fn
+        return -1;
+    } else {
+         while(Edges_ >> tempWord){ 
+            int ID = std::stoi(tempWord); // using stoi to convert string to int 
+
+            Edges_>>tempWord; //get our start/end nodes and store them
+            sourceNode = std::stoi(tempWord);
+            Edges_>>tempWord;
+            destinationNode = std::stoi(tempWord);
+
+            Edges_>>tempWord;
+            double distanceWeight = std::stod(tempWord); //using stod to convert string to double for our edge weight
+
+            Edges_>>tempWord;
+            int type = std::stoi(tempWord); // using stoi to convert string to int to get our routeType
+            
+            fullG_.insertEdge(ID, sourceNode, destinationNode, distanceWeight, type); //inserts our edge with the given data
+            i++;
+         }
+    }
+
+    Edges_.close(); // close the current file at reading
+    return i-1; // this returns the last index of our edge, if it is negative one then no edges were added
+}
+
+void fullGraph::connectGraphs() {
+
+}
+
+
+
+int fullGraph::getTransport1Start() {
+    return transport1_start;
+}
+
+int fullGraph::getTransport1End() {
+    return allPoints.size()-1;
+}
+
+int fullGraph::getTransport2Start() {
+    return 0;
+}
+
+int fullGraph::getTransport2End() {
+    return transport1_start;
 }
--- src/fileParser.h
+++ src/fileParser.h
@@ -5,20 +5,52 @@
 #include <fstream>
 #include <string>
 #include <utility>      // std::pair, std::make_pair
+#include <map>
 
 class fullGraph{
     public:
 
     /*
-    * @brief aconstructor for the fileParser
+    * @brief a constructor for the fileParser
     * This will take in a file with the given data type and create two connected graphs from it
     * It will also construct a KD-tree of the longitude/latitude points in radians to be used in connectGraphs();
     * 
-    * @param transport1 - the file name that holds the data for our vertices that represent our first travel type
-    * @param transport2 - the file name that holds the data for our vertices that represent our second travel type
-    * @param travelEdge - the file name that holds the data for our edges that connect our 2nd travel type together
+    * @param transport1 - the file name that holds the data for our nodes that represent our first travel type
+    * @param transport2 - the file name that holds the data for our nodes that represent our second travel type
+    * @param travelEdge1 - the file name that holds the data for our edges that connect our 1st travel type together
+    * @param travelEdge2 - the file name that holds the data for our edges of the 2nd data type
     */
-    fullGraph(const std::string& transport1, const std::string& transport2, const std::string& travelEdge );
+    fullGraph(const std::string& transport1, const std::string& transport2, const std::string& travelEdge1, const std::string& travelEdge2 );
+
+    /*
+    * @brief will be used by the constructor, it will take in a file with all the Nodes and insert them all in the graph.
+    * It will return an integer value of the index of the last element that was inserted. If the return value is -1, then no items were inserted
+    * 
+    * @param nodefile the file name that holds the data for our nodes
+    */
+    int insertAllNodes(const std::string& nodefile, bool needMap);
+
+    /*
+    * @brief will be used by the constructor, it will take in a file with all the Edges and insert them all in the graph with the proper weidght.
+    * It will return an integer value of the index of the last element that was inserted. If the return value is -1, then no items were inserted
+    * 
+    * @param edgefile the file name that holds the data for our edges
+    * @param needMap a boolean that keeps track if we need to map each longitude/latitude to an ID for later use in the kd tree
+    */
+    int insertAllEdges(const std::string& edgefile);
+
+    /*
+    * These set of four fns give the start/end index of their respective node type 
+    * This can be used to get the longitude/lattiude from the allPoints
+    */
+
+    int getTransport1Start();
+
+    int getTransport1End();
+
+    int getTransport2Start();
+
+    int getTransport2End();
 
 
     /*
@@ -29,8 +61,11 @@ class fullGraph{
     void connectGraphs();
 
     private:
-        Graph fullGraph_;
+        Graph fullG_;
+        std::vector<std::pair<double,double>> allPoints; // A vector to hold our nodes 
+        int transport1_start;
+        int transport2_end;
+        std::map<std::pair<double,double>, int> coorToID; // a map that relates our coordinate of long/lat to its unique id
        // KDTree lat_longTree_; // currently commented out untill kdTree.h/hpp is implemented
 };
-
 
