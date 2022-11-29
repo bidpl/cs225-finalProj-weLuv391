74521f2a89970537acbb09fe5d002fdebf50c7bc
nuoyanw2 <117323476+nuoyanw2@users.noreply.github.com>
2022-11-28 23:09:07 -0600

    convert "main(cleaning)" function into callable

---
+++ getCleanedData.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <istream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <fstream>
+#include <cmath>
+#include <sstream>
+
+using namespace std;
+void getCleanedData();
