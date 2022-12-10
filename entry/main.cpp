8feb3d6e97d72350562096ace1870e5862223042
Binh-Minh Nguyen <binhminh292@gmail.com>
2022-12-10 04:25:08 +0000

    Added a* call to main, buggy atm

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 #include "graph.h"
 #include "getCleanedData.h"
 #include "fileParser.h"
-#include <algorithm>
-// #include "a*.h"
+#include "a_star.h"
 
 // <command name> <road nodes/intersections> <road edges> <airport filename> 
 int main(int argc, char *argv[]) {
@@ -19,6 +20,9 @@ int main(int argc, char *argv[]) {
     // Build graph from edges and nodes file
     Graph network;
     fullGraph fileParser = fullGraph(std::vector<string>{"../src/nodes.txt", "../src/edges.txt"}, network);
+
+    std::vector<double> speeds = {60, 500}; // 60mph average highway, 500 mph cruising speed of 737
+    network.setSpeedLookup(speeds);
 
     std::cout << "Num nodes: " << network.getNodes().size() << std::endl;
     std::cout << "Num edges: " << network.getEdgeList().size() << std::endl << std::endl;
@@ -59,10 +63,11 @@ int main(int argc, char *argv[]) {
     std::cout << "BFS of MST contains " << counter << " nodes" << std::endl << std::endl;
 
     // Golden Gate S Vista Point to Santa Monica Pier
-    // std::vector<Graph::Edge> pathEdges = shortestPath(network, std::pair<double, double>{37.807680, -122.474735}, std::pair<double, double>{37.807680, -122.474735});
+    std::vector<Graph::Edge> pathEdges = shortestPath(network, std::pair<double, double>{-122.133544, 37.399267}, std::pair<double, double>{-122.097167, 37.421674});
 
     // some function to print edges out
     // function to print nodes in path in visited order
+    std::cout << "A* traversal includes: " << pathEdges.size() << std::endl << std::endl;
 
     return 0;
 }
